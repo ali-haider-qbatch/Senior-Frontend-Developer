734683d4cb6ef18ade18f428b62a9f3ac39f49e3
ateeq-rehman-qbatch <ateeq.rehman@qbatch.com>
2023-12-08 18:28:12 +0500

    make 3 section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import theme from './config/theme/themeVariables'
-import GlobalStyles from './config/theme/global.styled'
-import Header from './component/Header'
-import HeaderBanner from './component/HeaderBanner'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./config/theme/themeVariables";
+import GlobalStyles from "./config/theme/global.styled";
+
+import Header from "./component/Header";
+import HeaderBanner from "./component/HeaderBanner";
+import TrustedCompanies from "./components/TrustedCompanies";
+import CloudSoftware from "./components/CloudSoftware";
+import Testimonials from "./components/Testimonials";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      {' '}
       <GlobalStyles />
       <Header />
       <HeaderBanner />
+      <TrustedCompanies />
+      <CloudSoftware />
+      <Testimonials />
     </ThemeProvider>
-  )
+  );
 }
-
-export default App
+export default App;
